a66c27aed8f4a84d2206806adbec8ee1508a741e
Johannes Wolfgruber <johanneswolfgruber@web.de>
2017-06-13 21:55:45 +0200

    Set max audio streams to 2

--- app/src/main/java/com/johanneswolfgruber/learntolisten/Sound.java
+++ app/src/main/java/com/johanneswolfgruber/learntolisten/Sound.java
@@ -28,13 +28,13 @@ class Sound {
                     .setUsage(AudioAttributes.USAGE_GAME)
                     .build();
             mSoundPool = new SoundPool.Builder()
-                    .setMaxStreams(3)
+                    .setMaxStreams(2)
                     .setAudioAttributes(attributes)
                     .build();
 
             loadSounds(context);
         } else {
-            mSoundPool = new SoundPool(3, AudioManager.STREAM_MUSIC, 0);
+            mSoundPool = new SoundPool(2, AudioManager.STREAM_MUSIC, 0);
             loadSounds(context);
         }
 
